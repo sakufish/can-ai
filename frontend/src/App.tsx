773b8a8f516a834157cde3870bf4b85bff1a6625
caden c <cadenc.woss@gmail.com>
2025-05-31 23:31:12 -0400

    fix for vercel and render deployments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import Map from './pages/Map';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/temp" element={<Home />} />
         <Route path="/" element={<Map />} />
       </Routes>
     </Router>
