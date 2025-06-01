aaec076868da993aeb7099a25ee8afbf5b2689c6
caden c <cadenc.woss@gmail.com>
2025-05-31 23:04:27 -0400

    fix title upload images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Map from './pages/Map'; // example additional page
+import Map from './pages/Map';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
+        <Route path="/temp" element={<Home />} />
+        <Route path="/" element={<Map />} />
       </Routes>
     </Router>
   );
